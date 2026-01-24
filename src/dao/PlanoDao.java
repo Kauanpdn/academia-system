21fef3883e84a4bd99a8eebb0ac2e5ee232e3a47
OkauanDev <teko4278@gmail.com>
2026-01-24 17:04:02 -0300

    finalizando matriculaDao

--- src/dao/PlanoDao.java
+++ src/dao/PlanoDao.java
@@ -25,5 +25,68 @@ public class PlanoDao {
             throw new RuntimeException("Erro ao salvar plano! ", e);
         }
     }
-    
-}
+
+    //SELECT FOR ID
+    public Plano buscarPorId(int id){
+        String sql =  "SELECT * FROM plano WHERE id = ?";
+
+        try (Connection conn = DatabaseConnection.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)){
+            
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()){
+                Plano plano = new Plano();
+                plano.setId(rs.getInt("id"));
+                plano.setNome(rs.getString("nome"));
+                plano.setValor(rs.getDouble("valor"));
+                plano.setDuracaoMeses(rs.getInt("duracaoMeses"));
+                plano.setDescricao(rs.getString("descricao"));
+                return plano;
+            }
+
+            return null;
+
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao buscar aluno por ID", e);
+        }
+    }
+
+    //SELECT ALL
+    public List<Plano> listarTodos(){
+        String sql = "SELECT * FROM plano";
+        List<Plano> planos = new ArrayList<>();
+
+        try (Connection conn = DatabaseConnection.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql);
+                ResultSet rs = stmt.executeQuery();
+    ){
+            while (rs.next()) {
+                Plano plano = new Plano();
+                plano.setId(rs.getInt("id"));
+                plano.setNome(rs.getString("nome"));
+                plano.setValor(rs.getDouble("valor"));
+                plano.setDuracaoMeses(rs.getInt("duracaoMeses"));
+                plano.setDescricao(rs.getString("descricao"));
+            }
+
+            return planos;
+            
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao listar alunos ", e);
+        }
+    }
+
+    //DELETE
+    public void deletar(int id){
+        String sql = "DELETE FROM plano WHERE id = ?";
+
+            try (Connection conn = DatabaseConnection.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)){
+                
+                stmt.setInt(1, id);
+                stmt.executeUpdate();
+
+            } catch (Exception e) {
+                throw new RuntimeException("Erro ao deletar plano ", e);
+            }
+        }
+    }    
